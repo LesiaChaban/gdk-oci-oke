032fcda9f9c39b03900290fc749bfbe0975e9c90
Sachin Pikle <sachin.pikle@oracle.com>
2025-02-17 14:20:26 +0530

    GDK 4.7.3.1 update

--- gdk-object-store-oke-workload-17/oci/src/test/java/com/example/OciTest.java
+++ gdk-object-store-oke-workload-17/oci/src/test/java/com/example/OciTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Oracle and/or its affiliates
+ * Copyright 2025 Oracle and/or its affiliates
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ import org.junit.jupiter.api.Assertions;
 import jakarta.inject.Inject;
 
 @MicronautTest
-class GdkObjectStoreOkeWorkloadOciTest {
+class GdkOsOkeOciTest {
 
     @Inject
     EmbeddedApplication<?> application;
